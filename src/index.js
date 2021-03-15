6823a105486a5c63889f21c80352713a27f31d47
vino <vino0908@outlook.com>
2021-03-15 22:42:46 +0800

    添加代理

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //需求1：n不能小于0
-let data = {}
+const data = {}
 data._n = 0
 Object.defineProperty(data, 'n', {
     get() {
@@ -11,8 +11,31 @@ Object.defineProperty(data, 'n', {
     }
 })
 
-console.log(data.n)
+console.log(data.n) //0
 data.n = -1
-console.log(data.n)
+console.log(data.n) //0
 
 
+//需求2：如果直接修改data._n还是可以修改数据，使用代理解决这个问题
+
+const data2 = proxy({ data: { n: 0 } })
+
+//代理
+function proxy(options) {
+    const { data } = options
+    const obj = {}
+    Object.defineProperty(obj, 'n', {
+        get() {
+            return data.n
+        },
+        set(value) {
+            if (value < 0) return
+            data.n = value
+        }
+    })
+    return obj
+}
+
+console.log(data2.n) //0
+data2.n = -1
+console.log(data2.n) //0
