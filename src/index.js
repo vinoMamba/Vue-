c2b8cfded3076b5d9ae8156f03348e336590141c
vino <vino0908@outlook.com>
2021-03-15 22:59:43 +0800

    添加监听

--- src/index.js
+++ src/index.js
@@ -39,3 +39,41 @@ function proxy(options) {
 console.log(data2.n) //0
 data2.n = -1
 console.log(data2.n) //0
+
+
+//需求3： 如下注释可绕过代理.如果绕过代理就进行拦截
+/**
+ * const xxx=  {n:0}
+ * const data = proxy({data:xxx})
+ * xxx.n =  -1
+ */
+
+const dataValue = { n: 0 }
+const data3 = proxy2({ data: dataValue })
+
+function proxy2({ data }) {
+    //增加监听
+    const value = data.n
+    delete data.n //可以不写
+    Object.defineProperty(data, 'n', {
+        get() {
+            return value
+        },
+        set(newValue) {
+            if (newValue < 0) return
+            data.n = newValue
+        }
+    })
+    //代理
+    const obj = {}
+    Object.defineProperty(obj, 'n', {
+        get() {
+            return data.n
+        },
+        set(value) {
+            if (value < 0) return
+            data.n = value
+        }
+    })
+    return obj
+}
